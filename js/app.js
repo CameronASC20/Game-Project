13e9fd138cba9c9c1b596ed2b5cc5f292d4f00ac
CameronASC20 <camerongarnier73@gmail.com>
2022-02-25 17:05:56 -0500

    Hit detection does not work

--- js/app.js
+++ js/app.js
@@ -13,6 +13,7 @@ class Player {
         this.color = color,
         this.height = height,
         this.width = width
+        // this.jump
 		// this.direction = {
         //     up: false
         // }
@@ -21,7 +22,7 @@ class Player {
         // ctx.fillStyle will determine the color(or style) of your element
         ctx.fillStyle = this.color
         // ctx.fillRect will draw a rectangle on the canvas
-        ctx.fillRect(this.x, this.y, this.height, this.width)
+        ctx.fillRect(this.x, this.y, this.width, this.height)
     }
 }
 
@@ -38,10 +39,10 @@ class Block {
         // ctx.fillStyle will determine the color(or style) of your element
         ctx.fillStyle = this.color
         // ctx.fillRect will draw a rectangle on the canvas
-        ctx.fillRect(this.x, this.y, this.height, this.width)
+        ctx.fillRect(this.x, this.y, this.width, this.height)
         // ctx.translate(1500, 0)
         this.x -= this.speed;
-        if (this.x <= 0) {
+        if (this.x < 0) {
             console.log('x position is', this.x)
             this.x = canvas.width - this.width
         }
@@ -61,15 +62,30 @@ let block = new Block(1550, 450, 'lightgreen', 50, 50)
 const jump = (e) => {
     console.log('jump event', e)
     if (e.keyCode == 32) {
-            player.y -= 70
+        let start = Date.now()
+        player.y -= 100
+        const timer = setInterval(function() {
+            // how much time passed from the start?
+            console.log('timer')
+            let timePassed = Date.now() - start
+            
+            if (timePassed >= 1000) {
+                player.y += 100
+                console.log('2 seconds')
+                // finish animation after 2 sec
+                clearInterval(timer)
+            }
+        }, 100)
     }
 }
 
-const removeJump = () => {
-    if (player.y != 430) {
-        player.y += 70
-    }
-}
+
+// const removeJump = () => {
+//     if (player.y === 360) {
+//             player.y += 70 
+//     }
+//     console.log('player y', player.y)
+// }
 
 document.addEventListener('keydown', jump)
 
@@ -78,28 +94,25 @@ const gameLoop = () => {
     ctx.clearRect(0, 0, game.width, game.height)
     player.render()
     block.render()
-    removeJump
+    detectHit()
+    // removeJump()
+    // get timer at time of jump button pressed
+    // move player to jumping position
+    // check timer position against first
+    // if more than 10 has past reset player position
+    // reset jump timer
+}
+
+const detectHit = () => {
+    if (player.x + player.width > block.x &&
+        player.y >= block.y + block.height) {
+        console.log('hit')
+        document.getElementById('status').textContent = 'YOU LOSE'
+    }
 }
 
 let gameInterval = setInterval(gameLoop, 60)
 
 const stopGameLoop = () => {clearInterval(gameInterval)}
-// const removeJump = () => {
-//     setTimeout(function() {
-//         player.classList.remove('animate')
-//     }, 500)
-// }
 
 
-// const detectHit = () => {
-//     if (player.x < block.x + block.width) {
-//         alert('You Lose')
-//     }
-// }
-
-// document.body.onkeyup = function (e) {
-//     if (e.keyCode === 32) {
-//         jump()
-//         removeJump()
-//     }
-// }
