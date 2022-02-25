3b04cc514d32c14adb40db0b6086817309ef1e8b
CameronASC20 <camerongarnier73@gmail.com>
2022-02-25 14:00:15 -0500

    Added block and jump animation

--- js/app.js
+++ js/app.js
@@ -13,9 +13,9 @@ class Player {
         this.color = color,
         this.height = height,
         this.width = width
-		this.direction = {
-            up: false
-        }
+		// this.direction = {
+        //     up: false
+        // }
     }
     render = function () {
         // ctx.fillStyle will determine the color(or style) of your element
@@ -39,46 +39,45 @@ class Block {
         ctx.fillStyle = this.color
         // ctx.fillRect will draw a rectangle on the canvas
         ctx.fillRect(this.x, this.y, this.height, this.width)
+        // ctx.translate(1500, 0)
+        this.x -= this.speed;
+        if (this.x <= 0) {
+            console.log('x position is', this.x)
+            this.x = canvas.width - this.width
+        }
+        // requestAnimationFrame(block)
     }
 }
 
 let player = new Player(0, 430, 'blue', 50, 70)
 let block = new Block(1550, 450, 'lightgreen', 50, 50)
 
-const blockMove = () => {
-    if (x > canvas.width) {
-        x -= 100;
+
+
+
+
+
+
+const jump = (e) => {
+    console.log('jump event', e)
+    if (e.keyCode == 32) {
+        console.log('did this fire?')
+            player.y -= 50
     }
-    requestAnimationFrame(block)
 }
 
-// const stopGameLoop = () => {clearInterval(gameInterval)}
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     gameInterval
-// })
-
-// const jump = () => {
-//     switch (e.keycode) {
-//         case (32):
-//             player.y -= 10
-//             break
-//     }
-// }
-
-player.render()
-block.render()
-blockMove()
-
-// const gameLoop = () => {
-//     ctx.clearRect(0, 0, game.width, game.height)
-//     for (block = 0; block < 10; block++) {
-//         stopGameLoop()
-//         document.querySelector('#status').textContent = 'You Win!'
-//     }
-// }
+document.addEventListener('keydown', jump)
 
 
+const gameLoop = () => {
+    ctx.clearRect(0, 0, game.width, game.height)
+    player.render()
+    block.render()
+}
+
+let gameInterval = setInterval(gameLoop, 60)
+
+const stopGameLoop = () => {clearInterval(gameInterval)}
 // const removeJump = () => {
 //     setTimeout(function() {
 //         player.classList.remove('animate')
